b557ed752567571eb776e919f29ff017eddaee0e
FarruxAhmadov <105041270+FarruxAhmadov@users.noreply.github.com>
2022-06-28 11:43:51 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,22 @@
 const prompt = require('prompt');
 prompt.start();
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+prompt.get("input", function(err, result) {
+    let str = result.input;
+    let a = 0;
+    let b = 0;
+    for (i = 0; i < str.length; i++) {
+        if (parseInt(str[i]) == 2) {
+            a = a + 1;
+        } else if (parseInt(str[i]) === 5) {
+            b = b + 1;
+        }
+    }
+    if (a > b) {
+        console.log("2")
+    } else if (a < b) {
+        console.log("5")
+    } else {
+        console.log("=");
+    }
 });
