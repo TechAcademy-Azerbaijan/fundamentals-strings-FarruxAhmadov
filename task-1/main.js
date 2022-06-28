bedf4a2911b066c2672db5b748591222c71e4064
FarruxAhmadov <105041270+FarruxAhmadov@users.noreply.github.com>
2022-06-28 10:40:15 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,4 +1,11 @@
-let a = 31;
-let b = 15;
-let c = 40;
-console.log(Math.min(a, b, c)
+const prompt = require('prompt');
+prompt.start();
+
+prompt.get('input', function(err, result) {
+    let input = result.input;
+    let numbers = input.split(',')
+    let a = parseInt(numbers[0]);
+    let b = parseInt(numbers[1]);
+    let c = parseInt(numbers[2]);
+    console.log(Math.min(Math.min(a, b), c));
+});
